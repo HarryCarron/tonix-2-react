88ea679b8998c1a8172ec0c533fec29c7a3ace0d
Harry Carron <hcarron92@gmail.com>
2022-01-02 14:29:40 +0000

    animation class, signal css updates

---
+++ src/Utilities/AnimationStages.js
@@ -0,0 +1,48 @@
+export class Animation {
+
+    fps;
+    turn;
+    draw;
+
+    stop = false;
+    frameCount = 0;
+    fpsInterval;
+    startTime;
+    now;
+    then;
+    elapsed;
+
+    constructor(fps, turns, draw) {
+        this.fps = fps;
+        this.turns = turns;
+        this.draw = draw;
+    }
+
+    startAnimating() {
+        this.fpsInterval = 1000 / this.fps;
+        this.then = window.performance.now();
+        this.startTime = this.then;
+        console.log(this.startTime);
+        this.animate();
+    }
+
+    animate(newtime) {
+
+        if (this.stop) {
+            return;
+        }
+        requestAnimationFrame(this.animate);
+
+        this.now = newtime;
+        this.elapsed = this.now - this.then;
+
+        if (this.elapsed > this.psInterval) {
+
+            this.then = this.now - (this.elapsed % this.fpsInterval);
+    
+            this.draw();
+
+        }
+    }
+    
+}
